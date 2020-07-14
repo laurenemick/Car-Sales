5e31d17d72ee0bf37b4d216864d979be335e2ff9
Lauren Emick <laurenemick6@gmail.com>
2020-07-14 14:55:14 -0700

    adding mapStateToProps

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 const AddedFeature = props => {
   return (
@@ -10,4 +11,14 @@ const AddedFeature = props => {
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  console.log(state);
+  // return {
+ 
+  // };
+};
+
+export default connect(
+  mapStateToProps, 
+  { } 
+)(AddedFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import { featureReducer } from "./reducers/featureReducer";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featureReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
